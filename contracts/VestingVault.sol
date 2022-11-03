4f4a5923bf72266b934e244571c239a6bc1e1b31
Filip Kirin <kirin@pop-os.localdomain>
2022-11-03 08:14:25 +0100

    added function getTimeUntilUnlocked

--- contracts/VestingVault.sol
+++ contracts/VestingVault.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-error AlreadyFundedError(bool locked);
 error BeneficiaryZeroAddressError(address beneficiary);
+error AlreadyFundedError(bool funded);
 error TokenZeroAddressError(address token);
 error InsufficientFundAmountError(uint256 amount);
 error InsufficientLockedTimeError(uint256 lockedTime);
@@ -16,19 +16,14 @@ error UnlockTimeNotPassedError();
 contract VestingVault is Ownable{
     // event EtherFunded(uint256 amount);
     // event EtherWithdrawn(uint256 amount);
-    event ERC20Funded(address token ,uint256 amount);
-    event ERC20RWithdrawn(address token, uint256 amount);
+    event ERC20Funded(address token, uint256 amount);
+    event ERC20Withdrawn(address token, uint256 amount);
 
     address private immutable _beneficiary;
     address public tokenVestedAddress;
     uint256 public amountVested;
     uint256 public unlockTime;
-    bool public locked;
-
-    modifier isLocked() {
-        if(locked) revert AlreadyFundedError(locked);
-        _;
-    }
+    bool public funded = false;
 
     constructor(address beneficiary) {
         if(beneficiary == address(0)) revert BeneficiaryZeroAddressError(beneficiary);
@@ -47,7 +42,12 @@ contract VestingVault is Ownable{
         return unlockTime;
     }
 
-    function fund(address tokenAddress, uint256 amount, uint256 lockedTime) public onlyOwner isLocked {
+    function getTimeUntilUnlocked() public view returns (uint256) {
+        return unlockTime - block.timestamp;
+    }
+
+    function fund(address tokenAddress, uint256 amount, uint256 lockedTime) public onlyOwner {
+        if(funded) revert AlreadyFundedError(funded);
         if(tokenAddress == address(0)) revert TokenZeroAddressError(address(tokenAddress));
         if(amount <= 0) revert InsufficientFundAmountError(amount);
         if(lockedTime <= 0) revert InsufficientLockedTimeError(lockedTime);
@@ -56,7 +56,7 @@ contract VestingVault is Ownable{
         IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount);
         emit ERC20Funded(tokenAddress, amount);
 
-        locked = true;
+        funded = true;
         tokenVestedAddress = tokenAddress;
         amountVested = amount;
         unlockTime = block.timestamp + lockedTime;
@@ -64,10 +64,10 @@ contract VestingVault is Ownable{
 
     function withdraw() public {
         if(msg.sender != getBeneficiary()) revert WithdrawerNotBeneficiaryError(getBeneficiary());
-        if(!locked) revert VaultNotFundedError();
+        if(!funded) revert VaultNotFundedError();
         if(block.timestamp < unlockTime) revert UnlockTimeNotPassedError();
 
         IERC20(tokenVestedAddress).transfer(getBeneficiary(), amountVested);
-        emit ERC20RWithdrawn(tokenVestedAddress, amountVested);
+        emit ERC20Withdrawn(tokenVestedAddress, amountVested);
     }
 }
