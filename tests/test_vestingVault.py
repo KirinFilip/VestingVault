01c4f6a270d33d17e37d87eab97982cc06f06e6e
KirinFilip <filip.kirin.zg1@gmail.com>
2022-11-10 15:57:44 +0100

    added tests for withdrawToken function

--- tests/test_vestingVault.py
+++ tests/test_vestingVault.py
@@ -66,17 +66,17 @@ def test_InsufficientLockedTime(vaultContract, tokenContract):
         assert "VestingVault__InsufficientLockedTime" in str(ex)
 
 
-#### withdraw function ###
+#### withdrawToken function ###
 
 
-def test_withdraw(vaultContract, tokenContract):
+def test_withdrawToken(vaultContract, tokenContract):
     assert vaultContract.funded() == False
     tokenContract.transfer(accounts[0], 10000, {"from": accounts[2]})
     tokenContract.approve(vaultContract.address, 10000, {"from": accounts[0]})
     vaultContract.fund(tokenContract.address, 100, 10, {"from": accounts[0]})
     assert vaultContract.funded() == True
     chain.sleep(10)
-    vaultContract.withdraw({"from": accounts[1]})
+    vaultContract.withdrawToken({"from": accounts[1]})
     assert tokenContract.balanceOf(accounts[1]) == 100
 
 
@@ -88,19 +88,14 @@ def test_WithdrawerNotBeneficiary(vaultContract, tokenContract):
     assert vaultContract.funded() == True
     chain.sleep(10)
     try:
-        vaultContract.withdraw({"from": accounts[1]})
+        vaultContract.withdrawToken({"from": accounts[1]})
     except exceptions.VirtualMachineError as ex:
         assert "VestingVault__WithdrawerNotBeneficiary" in str(ex)
 
 
 # it raises VestingVault__AlreadyFunded not VestingVault__VaultNotFunded
 def test_VaultNotFunded(vaultContract):
-    assert vaultContract.funded() == False
-    chain.sleep(10)
-    try:
-        vaultContract.withdraw({"from": accounts[1]})
-    except exceptions.VirtualMachineError as ex:
-        assert "VestingVault__VaultNotFunded" in str(ex)
+    vaultContract.withdrawToken({"from": accounts[1]})
 
 
 def test_UnlockTimeNotPassed(vaultContract, tokenContract):
@@ -110,7 +105,7 @@ def test_UnlockTimeNotPassed(vaultContract, tokenContract):
     vaultContract.fund(tokenContract.address, 100, 10, {"from": accounts[0]})
     assert vaultContract.funded() == True
     try:
-        vaultContract.withdraw({"from": accounts[1]})
+        vaultContract.withdrawToken({"from": accounts[1]})
     except exceptions.VirtualMachineError as ex:
         assert "VestingVault__UnlockTimeNotPassed" in str(ex)
 
